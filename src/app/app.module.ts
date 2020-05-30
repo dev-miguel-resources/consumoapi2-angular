25b5837fb97ad9f86ff776615c1087723a216e52
dev-miguel-resources <ingenieromiguelch@gmail.com>
2020-05-30 12:31:58 -0400

    optimizando cargas avanzado

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgxPaginationModule} from 'ngx-pagination';
+import { LayoutComponent } from './components/layout/layout.component';
+import { MaterialModule } from './components/material/material/material.module';
+import { PageNotFoundModule } from './components/page-not-found/page-not-found.module';
+import { SharedModule } from './shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    NgxPaginationModule,
+    MaterialModule, 
+    SharedModule,
+    FormsModule, 
+    PageNotFoundModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
